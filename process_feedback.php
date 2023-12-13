3ac6afaae22d7f2943fb9aee61d45f747b7a6829
Bravine E. Bwire <bravinebwire98@gmail.com>
2023-12-13 15:06:55 +0300

    update

--- process_feedback.php
+++ process_feedback.php
@@ -1,30 +1,11 @@
-<!-- <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Collect form data
-    $center_visited = $_POST["center-visited"];
-    $date = $_POST["date"];
-    $preference = $_POST["preference"];
-    $reasons = $_POST["reasons"];
-    $first_name = $_POST["first-name"];
-    $email = $_POST["email"];
-    $mobile_number = $_POST["number"];
+<?php
+$to = 'sarahodini@gmailcom';
+$subject = 'User Feedback';
+$rating = $_POST['rating'];
+$feedback = $_POST['feedback'];
 
-    // Format the email content
-    $to = "bravinebwire98@gmail.com";
-    $subject = "Customer Feedback";
-    $message = "Medical Center Visited: $center_visited\n";
-    $message .= "Date: $date\n";
-    $message .= "How likely are you to recommend us: $preference\n";
-    $message .= "Reasons: $reasons\n";
-    $message .= "First Name: $first_name\n";
-    $message .= "Email: $email\n";
-    $message .= "Mobile Number: $mobile_number\n";
+$message = "Rating: $rating\n\nFeedback: $feedback";
+$headers = "From: sarahodini@gmail.com"; // Replace with your email address
 
-    // Send the email
-    if (mail($to, $subject, $message)) {
-        echo "Thank you for your feedback!";
-    } else {
-        echo "Oops! Something went wrong. Please try again later.";
-    }
-}
-?> -->
+mail($to, $subject, $message, $headers);
+?>
